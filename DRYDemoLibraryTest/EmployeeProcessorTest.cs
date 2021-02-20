aa9ef1156504a92de4d1d781e743d838f80836d2
Farrukh Inomov <farrukhfinomov@gmail.com>
2021-02-20 14:50:19 +0500

    created a project for testing the DRYDemoLibrary and wrote a test for a case

---
+++ DRYDemoLibraryTest/EmployeeProcessorTest.cs
@@ -0,0 +1,28 @@
+﻿using DRYDemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DRYDemoLibraryTest
+{
+    public class EmployeeProcessorTest
+    {
+        [Theory]
+        [InlineData("Farrukh", "Inomov", "FarrInom")]
+        public void GenerateEmployeeID_ShouldCalculate(string firstName, string lastName, string expectedStart)
+        {
+            // Arrange
+            EmployeeProcessor processor = new EmployeeProcessor();
+
+            // Act
+            string actualStart = processor.GenerateEmployeeID(firstName, lastName).Substring(0, 8);
+            
+            // Assert
+            Assert.Equal(expectedStart, actualStart);
+        }
+
+    }
+}
